b2e9f06231281ed779a58f1dd7af751ad36e6054
sarthak-patel12 <68072075+sarthak-patel12@users.noreply.github.com>
2024-03-10 00:18:24 +0530

    Update settings.py

--- marriage_photography/settings.py
+++ marriage_photography/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -88,10 +88,11 @@ WSGI_APPLICATION = 'marriage_photography.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
